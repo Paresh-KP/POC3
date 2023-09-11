1414bdd9d124e649b494eb81ab5e67f81ea88916
Paresh Kumar Prajapat <pkprajapat@q3infotech.net>
2023-09-11 22:12:36 +0530

    first serverless function

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const User = require('../../models/User');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const User = require('../../models/User');
 
-const salt = bcrypt.genSaltSync(10);
+// const salt = bcrypt.genSaltSync(10);
 
-exports.handler = async (event, context) => {
-  try {
-    const { username, password } = JSON.parse(event.body);
+// exports.handler = async (event, context) => {
+//   try {
+//     const { username, password } = JSON.parse(event.body);
 
    
-    await mongoose.connect(process.env.MONGODB_URI);
+//     await mongoose.connect(process.env.MONGODB_URI);
 
     
-    const userDoc = await User.create({
-      username,
-      password: bcrypt.hashSync(password, salt), 
-    });
+//     const userDoc = await User.create({
+//       username,
+//       password: bcrypt.hashSync(password, salt), 
+//     });
 
     
-    await mongoose.connection.close();
+//     await mongoose.connection.close();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(userDoc),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred' }),
-    };
-  }
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(userDoc),
+//     };
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'An error occurred' }),
+//     };
+//   }
+// };
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: 'hello world!',
+  };
 };
+
 
