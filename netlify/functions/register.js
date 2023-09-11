c24e4f53f39d584e021cbb2d49963856b6472db9
Paresh Kumar Prajapat <pkprajapat@q3infotech.net>
2023-09-11 21:21:39 +0530

    update1

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,33 +1,38 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('.../models/User');
+const User = require('../models/User');
+
 const salt = bcrypt.genSaltSync(10);
 
 exports.handler = async (event, context) => {
-  try {
-    const { username, password } = JSON.parse(event.body);
+  // try {
+  //   const { username, password } = JSON.parse(event.body);
 
    
-    await mongoose.connect(process.env.MONGODB_URI);
+  //   await mongoose.connect(process.env.MONGODB_URI);
 
     
-    const userDoc = await User.create({
-      username,
-      password: bcrypt.hashSync(password, salt), 
-    });
+  //   const userDoc = await User.create({
+  //     username,
+  //     password: bcrypt.hashSync(password, salt), 
+  //   });
 
     
-    await mongoose.connection.close();
+  //   await mongoose.connection.close();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(userDoc),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred' }),
-    };
-  }
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(userDoc),
+  //   };
+  // } catch (error) {
+  //   console.error(error);
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ error: 'An error occurred' }),
+  //   };
+  // }
+  return{
+    statusCode: 200,
+    body: JSON.stringify({ error: 'An error occurred' }),
+  };
 };
